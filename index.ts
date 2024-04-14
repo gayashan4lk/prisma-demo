54aa747ca43555731ac34b56a517454563407ae9
gayashan4lk <gayashan4lk@gmail.com>
2024-04-15 04:09:51 +0530

    query the db

--- index.ts
+++ index.ts
@@ -3,27 +3,49 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient()
 
 async function main() {
-	await prisma.user.create({
-		data: {
-			name: 'Alice',
-			email: 'alice@prisma.io',
-			posts: {
-				create: { title: 'Hello world' },
-			},
-			profile: {
-				create: { bio: 'I like spaceships.' },
-			},
-		},
-	})
+	// Querying the database
+	// STEP 01
+	// const allUsers = await prisma.user.findMany()
+  // console.log(allUsers)
 
-	const allUsers = await prisma.user.findMany({
-		include: {
-			posts: true,
-			profile: true,
-		},
-	})
-	// console.log(allUsers)
-	console.dir(allUsers, { depth: null})
+	// STEP 02
+	// await prisma.user.create({
+	// 	data: {
+	// 		name: 'Alice',
+	// 		email: 'alice@prisma.io',
+	// 		posts: {
+	// 			create: { title: 'Hello world' },
+	// 		},
+	// 		profile: {
+	// 			create: { bio: 'I like spaceships.' },
+	// 		},
+	// 	},
+	// })
+	// const allUsers = await prisma.user.findMany({
+	// 	include: {
+	// 		posts: true,
+	// 		profile: true,
+	// 	},
+	// })
+	// console.dir(allUsers, { depth: null})
+
+	// STEP 03
+	// const post = await prisma.post.update({
+	// 	where: {id: 1},
+	// 	data: {published: true},
+	// })
+	// console.log(post)
+
+	// STEP 04
+	// const user = await prisma.user.findFirst({
+	// 	where: {id: 1},
+	// 	include: {
+	// 		posts: true,
+	// 		profile: true,
+	// 	}
+	// })
+	// console.log(user);
+
 }
 
 main().then(async () => {
