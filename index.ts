7e3c6294bd495bd4ed26bf5d3a5372701e667a43
gayashan4lk <gayashan4lk@gmail.com>
2024-04-15 04:02:16 +0530

    create a user

--- index.ts
+++ index.ts
@@ -3,8 +3,27 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient()
 
 async function main() {
-	const allUsers = await prisma.user.findMany()
-	console.log(allUsers)
+	await prisma.user.create({
+		data: {
+			name: 'Alice',
+			email: 'alice@prisma.io',
+			posts: {
+				create: { title: 'Hello world' },
+			},
+			profile: {
+				create: { bio: 'I like spaceships.' },
+			},
+		},
+	})
+
+	const allUsers = await prisma.user.findMany({
+		include: {
+			posts: true,
+			profile: true,
+		},
+	})
+	// console.log(allUsers)
+	console.dir(allUsers, { depth: null})
 }
 
 main().then(async () => {
